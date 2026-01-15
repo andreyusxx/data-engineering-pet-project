eb778525bc9f47610b665d3be844e24ef3483a09
Шарагін Андрій <andrijsaragin@gmail.com>
2026-01-15 15:51:51 +0200

    Fix: remove duplicate dwh database creation from init script

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,5 +1,5 @@
 
 CREATE DATABASE airflow_db;
-CREATE DATABASE dwh; -- про всяк випадок, якщо її немає за замовчуванням
+
 GRANT ALL PRIVILEGES ON DATABASE airflow_db TO dwh_user;
-GRANT ALL PRIVILEGES ON DATABASE dwh TO dwh_user;er;
+GRANT ALL PRIVILEGES ON DATABASE dwh TO dwh_user;
