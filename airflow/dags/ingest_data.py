6737c4e0f5e7c3d646ec9eb78ab5b2c01c36e527
Шарагін Андрій <andrijsaragin@gmail.com>
2026-01-16 15:07:52 +0200

    Feat: integrate dbt into airflow dag for full automation

--- airflow/dags/ingest_data.py
+++ airflow/dags/ingest_data.py
@@ -1,11 +1,11 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator  # <--- НОВИЙ ІМПОРТ
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 import pandas as pd
 import os
 
-# Налаштування
 DATA_PATH = '/opt/airflow/data'
 PG_CONN_ID = 'postgres_dwh'
 
@@ -18,25 +18,19 @@ default_args = {
 def load_csv_to_postgres(file_name, table_name):
     file_path = os.path.join(DATA_PATH, file_name)
     print(f"Reading file: {file_path}")
-    
-    # Читання і завантаження
     df = pd.read_csv(file_path)
     hook = PostgresHook(postgres_conn_id=PG_CONN_ID)
     engine = hook.get_sqlalchemy_engine()
-    
     with engine.connect() as connection:
         connection.execute(f"TRUNCATE TABLE raw.{table_name}")
-        print(f"Table raw.{table_name} truncated.")
-
     df.to_sql(table_name, engine, schema='raw', if_exists='append', index=False)
-    print(f"Loaded {len(df)} rows to {table_name}")
 
 with DAG(
-    dag_id='01_ingest_data',
+    dag_id='02_elt_pipeline',  
     default_args=default_args,
     schedule_interval=None,
     catchup=False,
-    tags=['ingestion']
+    tags=['elt']
 ) as dag:
 
     load_users = PythonOperator(
@@ -51,4 +45,11 @@ with DAG(
         op_kwargs={'file_name': 'orders.csv', 'table_name': 'orders'}
     )
 
-    load_users >> load_orders
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+
+        bash_command='cd /opt/airflow/dbt && dbt run --profiles-dir .'
+    )
+
+
+    [load_users, load_orders] >> dbt_run
